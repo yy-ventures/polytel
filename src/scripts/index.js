48760be8cecfb061dc8fe307306990a9d62a5c27
salman-abedin <salman104alvi@gmail.com>
2021-05-27 15:51:47 +0600

    added delay for sliding

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,6 @@
 import { add_class_on_focus, toggle_navigation_on_scroll } from './dom_utils'
 
 add_class_on_focus({
-
   ['.moved_right']: 'move_back',
   ['.moved_left']: 'move_back',
   ['.faded_out']: 'fade_in',

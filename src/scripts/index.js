22ff09c34665b7853804ddf23e27d4abad980042
salman-abedin <salman104alvi@gmail.com>
2021-05-27 14:06:35 +0600

    added animations

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
-// console.log('yo');
+import { add_class_on_focus, doms } from './dom_utils'
+
+document.querySelector('#intro img').onload = () => {
+  add_class_on_focus({
+    ['.moved_right']: 'move_back',
+    ['.moved_left']: 'move_back',
+    ['.faded_out']: 'fade_in',
+  });
+}

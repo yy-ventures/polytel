0ede1590d9c0d740ba9a58d25006cb3be7832616
salman-abedin <salman104alvi@gmail.com>
2021-05-27 16:25:17 +0600

    finalized things

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,17 @@
 import { add_class_on_focus, toggle_navigation_on_scroll } from './dom_utils'
 
-add_class_on_focus({
-  ['.moved_right']: 'move_back',
-  ['.moved_left']: 'move_back',
-  ['.faded_out']: 'fade_in',
-});
+// add_class_on_focus({
+//   ['.moved_right']: 'move_back',
+//   ['.moved_left']: 'move_back',
+//   ['.faded_out']: 'fade_in',
+// });
+
 toggle_navigation_on_scroll()
 
-// document.querySelector('#intro img').onload = () => {
-//   add_class_on_focus({
-//     ['.moved_right']: 'move_back',
-//     ['.moved_left']: 'move_back',
-//     ['.faded_out']: 'fade_in',
-//   });
-//   toggle_navigation_on_scroll()
-// }
+document.querySelector('#intro img').onload = () => {
+  add_class_on_focus({
+    ['.moved_right']: 'move_back',
+    ['.moved_left']: 'move_back',
+    ['.faded_out']: 'fade_in',
+  });
+}
